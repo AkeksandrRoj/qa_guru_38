654e6251dc7304e5e77745793cdc3fb6c75a00e9
Aleksandr Rozhkov <dir.gefest@gmail.com>
2025-10-04 22:34:59 +0300

    Add SimpleJUnitTest

---
+++ src/test/java/SearchTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SearchTest {
+
+
+    @Test
+    void successfulSearchTest() {
+        open("https://duckduckgo.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=r1-0]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
+
--- src/test/java/SimpleJUnitTest.java
+++ src/test/java/SimpleJUnitTest.java
@@ -1,15 +1,49 @@
-import org.junit.jupiter.api.Test;
-
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import org.junit.jupiter.api.*;
 
 public class SimpleJUnitTest {
-    @Test
-    void successfulSearchTest() {
-        open("https://duckduckgo.com/");
-        $("[name=q]").setValue("selenide").pressEnter();
-        $("[id=r1-0]").shouldHave(text("https://ru.selenide.org"));
-    }
-}
+    int result;
 
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("###   beforeAll()\n");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("###   beforeEach()");
+          result = getResult();
+        }
+        @AfterEach
+    void afterEach(){
+        System.out.println("###   afterEach()\n");
+        result = 0;
+    }
+
+        @Test
+     void firstTest(){
+        Assertions.assertTrue(result>2);
+        System.out.println("###   firstTest()");
+    }
+
+    @Test
+    void secondTest(){
+        Assertions.assertTrue(result>2);
+        System.out.println("###   secondTest()");
+    }
+
+    @Test
+    void thirdTest(){
+        Assertions.assertTrue(result>2);
+        System.out.println("###   thirdTest()");
+    }
+    @AfterAll
+    static void afterAll(){
+        System.out.println("###   afterAll()");
+    }
+
+
+    private int getResult() {
+        return 3;
+    }
+
+}
