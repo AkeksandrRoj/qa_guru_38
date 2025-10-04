409babca86c66c8450e506d974f6fa9a029280e1
Aleksandr Rozhkov <dir.gefest@gmail.com>
2025-10-04 21:31:38 +0300

    Add structure project, add successfulSearchTest

---
+++ src/test/java/Lesson_3/ClassWork/SimpleJUnitTest.java
@@ -0,0 +1,18 @@
+package Lesson_3.ClassWork;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SimpleJUnitTest {
+
+        @Test
+    void successfulSearchTest() {
+        open("https://duckduckgo.com/");
+        $("[name=q]").setValue("selenide").pressEnter();
+        $("[id=r1-0]").shouldHave(text("https://ru.selenide.org"));
+    }
+}
+
